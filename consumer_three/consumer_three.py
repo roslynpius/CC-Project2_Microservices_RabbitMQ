ff1135418f2e7909fdc307eabfa4683951b01697
roslynpius <79206594+roslynpius@users.noreply.github.com>
2023-04-26 00:53:44 +0530

    Create consumer_three.py

---
+++ consumer_three/consumer_three.py
@@ -0,0 +1,41 @@
+import pymongo
+import time
+import pika
+import json
+import logging
+logging.basicConfig(level=logging.INFO)
+mongo_client = pymongo.MongoClient("mongodb://database")
+db = mongo_client["studentdatabase"]
+collection = db["students"]
+
+def delete_student(srn):
+    query = {"srn": srn}
+    result = collection.delete_one(query)
+    return result.deleted_count > 0
+
+print('172.17.0.1')
+time.sleep(9)
+
+# Establish connection with RabbitMQ
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters('172.17.0.1',5672,'/',credentials,heartbeat=1200)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+# Declare queue and callback function
+channel.queue_declare(queue='delete_record')
+
+def callback(ch, method, properties, body):
+    data = json.loads(body)
+    logging.info("Received delete record message: %s",data)
+    srn = data['srn']
+    if delete_student(srn):
+        logging.info(f"Deleted record with SRN: %s",data)
+    else:
+        logging.info(f"Record with SRN %S not found",data)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+channel.basic_consume(queue='delete_record', on_message_callback=callback)
+print('Waiting for delete record messages...')
+channel.start_consuming()
